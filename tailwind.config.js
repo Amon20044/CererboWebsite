7e0eaba6afcb4711c0ca7b96acea03f22c98445b
sohamhaldar <sohamhaldar25@gmail.com>
2024-09-11 09:57:20 +0530

    Added Sponsors Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily:{
+        inter: ['Inter', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
