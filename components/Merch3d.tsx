0fff7a7bf508729e24636b6dd07f9be61f81ec63
Devyash Saini <dysaini2004@gmail.com>
2024-10-28 21:28:26 +0530

    merch check

---
+++ components/Merch3d.tsx
@@ -0,0 +1,13 @@
+'use client'
+import Spline from '@splinetool/react-spline';
+
+export default function Merch3d() {
+  return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/FR8xGuZu-Eva6IUy/scene.splinecode" 
+        style={{height: "500px"}}
+      />
+    </main>
+  );
+}
