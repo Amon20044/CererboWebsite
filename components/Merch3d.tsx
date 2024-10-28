b0c2b5adabf0feb20406b26ecfc0e4d7e3a515b8
Devyash Saini <dysaini2004@gmail.com>
2024-10-28 21:51:15 +0530

    merch position fixed

--- components/Merch3d.tsx
+++ components/Merch3d.tsx
@@ -7,6 +7,10 @@ export default function Merch3d() {
       <Spline
         scene="https://prod.spline.design/FR8xGuZu-Eva6IUy/scene.splinecode" 
         style={{height: "500px"}}
+        onLoad={(spline) => {
+          const tshirt = spline.findObjectByName("male_tshirt");
+          if (tshirt) {tshirt.position.x += 25;}
+        }}
       />
     </main>
   );
