f769cf96c54adc118118238a6cf01e5a695e5424
sohamhaldar <sohamhaldar25@gmail.com>
2024-10-29 20:39:49 +0530

    SEO Optimization

---
+++ app/sitemap.js
@@ -0,0 +1,29 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://cerebro2024.vercel.app/',
+        lastModified: new Date(), 
+      },
+      {
+        url: 'https://cerebro2024.vercel.app/sponsors',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://cerebro2024.vercel.app/team',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://cerebro2024.vercel.app/events',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://cerebro2024.vercel.app/merch',
+        lastModified: new Date(),
+      }
+      ,
+      {
+        url: 'https://cerebro2024.vercel.app/contactUs',
+        lastModified: new Date(),
+      }
+    ]
+  }
