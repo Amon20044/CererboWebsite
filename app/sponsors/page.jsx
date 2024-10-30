212ab7fe15f8ee546a2b816a0af592a26d65f3d8
sohamhaldar <sohamhaldar25@gmail.com>
2024-10-30 22:25:54 +0530

    Added Sponsors Images

--- app/sponsors/page.jsx
+++ app/sponsors/page.jsx
@@ -1,15 +1,23 @@
 import Image from "next/image";
 import React from "react";
 import SponsorCard from '../../components/SponsorCard';
+import Cubelelo from '../../assets/sponsorsPage/Cubelelo_Logo_HR4.png';
+import Evelelo_Light from '../../assets/sponsorsPage/Evepaper_Light.png';
+import JDoodle from '../../assets/sponsorsPage/JDoodle_Logo_JPEG.jpg';
+import Noticebard from '../../assets/sponsorsPage/noticebard.png';
+import SF from '../../assets/sponsorsPage/SF_Logo_White.png';
+import InterviewBuddy from '../../assets/sponsorsPage/InterviewBuddy_Logo_updated_White_BG.png'
 const sponsorPage=()=>{
 
     return(
         <div className="py-8 w-full h-auto bg-[#212121] container mx-auto px-4" >
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8 place-content-center">
-               <SponsorCard/> 
-               <SponsorCard/> 
-               <SponsorCard/> 
-               <SponsorCard/> 
+               <SponsorCard title="Cubelelo" Img={Cubelelo}/> 
+               <SponsorCard title="EVEPAPER" Img={Evelelo_Light}/> 
+               <SponsorCard title="JDoodle" Img={JDoodle} width={400}/> 
+               <SponsorCard title="Noticebard" Img={Noticebard} width={400}/> 
+               <SponsorCard title="Stubborn Factory" Img={SF}/> 
+               <SponsorCard title="Interview Buddy" Img={InterviewBuddy} width={400}/>  
             </div>
         </div>
     )
--- components/SponsorCard.jsx
+++ components/SponsorCard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import FigmaImg from '../assets/sponsorsPage/FigmaImg.svg';
 import Image from 'next/image';
 
-function SponsorCard() {
+function SponsorCard({title,Img,height,width}) {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl text-center tracking-wide font-elevon text-white">TITLE SPONSORS</h1>
-      <Image src={FigmaImg} width={200} height={100} alt="image" className="py-4 m-4" />
+    <div className="flex flex-col items-center justify-start">
+      <h1 className="text-4xl text-center tracking-wide font-elevon text-white">{title||"TITLE SPONSORS"}</h1>
+      <Image src={Img||FigmaImg} width={width||200} height={height||100} alt="image" className="py-4 m-4" />
     </div>
   );
 }
