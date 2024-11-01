6b7fb3ffd4348a4d296a8c0914b54b0d770bf37b
Devyash Saini <dysaini2004@gmail.com>
2024-11-01 16:40:02 +0530

    error check

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -40,7 +40,7 @@ function Team() {
         <div>
           <div className='flex flex-wrap justify-center mt-10 px-4 md:px-8'>
             {[ 
-              { name: "Vaibhav Jonwal", position: "GYMKHAHA" },
+              { name: "vaibhav", position: "GYMKHAHA" },
               { name: "Amon Sharma", position: "TECH. COMMITTEE" },
             ].map((profile, index) => (
               <motion.div
